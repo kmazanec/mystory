4af2c6c0f87f11b788b24f0609d9156ff5142469
kmazanec <k.mazanec@gmail.com>
2013-10-31 01:00:05 -0500

    add horizontal scrolling vandy image

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,5 @@
+var windowWidth = $(window).width();
+
 $(document).ready(function(){
   $('section[data-type="background"]').each(function(){
     var $bgobj = $(this); // assigning the object
@@ -7,12 +9,29 @@ $(document).ready(function(){
       var coords = '50% ' + yPos + 'px';
       $bgobj.css({ backgroundPosition: coords });
       Moon.move();
+      VandyPan.move();
     });
   });
 
   Moon.init();
+  VandyPan.init();
 
 });
+
+var VandyPan = {
+  init: function() {
+    this.$vandy = $('#vandy');
+    this.offset = this.$vandy.offset().top;
+  },
+  move: function(){
+    var xPos = this.offset - $(window).scrollTop();
+    if (xPos > 0 || xPos <= windowWidth - 3240) {
+      return false;
+    }
+    var coords = xPos+'px 0';
+    this.$vandy.css({ backgroundPosition: coords });
+  }
+};
 
 
 
@@ -22,13 +41,14 @@ var Moon = {
     // this.xPos = this.$moon.position().left;
     this.xPos = 0;
     this.yPos = 0;
-    this.windowWidth = $(window).width();
   },
   move: function(){
     var newXPos = this.xPos + $(window).scrollTop();
-    var modFactor = ($(window).scrollTop())/this.windowWidth;
+    if (newXPos > windowWidth){
+      return false;
+    }
+    var modFactor = ($(window).scrollTop()*0.8)/windowWidth;
     var newYPos = this.yPos + $(window).scrollTop()*modFactor;
-
     this.$moon.css({'left': newXPos+'px',
                     'top' : newYPos+'px'});
   }
