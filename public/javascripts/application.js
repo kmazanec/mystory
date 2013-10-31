a12dc1b1bc9afe0b5d6a7b2870d57ff31b397a91
kmazanec <k.mazanec@gmail.com>
2013-10-30 23:44:22 -0500

    add javascript parallax and moon movment

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,34 @@ $(document).ready(function(){
   $('section[data-type="background"]').each(function(){
     var $bgobj = $(this); // assigning the object
     $(window).scroll(function() {
+      var yMod = $bgobj[0].style.backgroundPositionY;
       var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
-      var coords = '50% '+ yPos + 'px';
+      var coords = '50% ' + yPos + 'px';
       $bgobj.css({ backgroundPosition: coords });
+      Moon.move();
     });
   });
+
+  Moon.init();
+
 });
+
+
+
+var Moon = {
+  init: function(){
+    this.$moon = $('#moon');
+    // this.xPos = this.$moon.position().left;
+    this.xPos = 0;
+    this.yPos = 0;
+    this.windowWidth = $(window).width();
+  },
+  move: function(){
+    var newXPos = this.xPos + $(window).scrollTop();
+    var modFactor = ($(window).scrollTop())/this.windowWidth;
+    var newYPos = this.yPos + $(window).scrollTop()*modFactor;
+
+    this.$moon.css({'left': newXPos+'px',
+                    'top' : newYPos+'px'});
+  }
+};
